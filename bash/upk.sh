dece97ea2ea726cbe2c67d0c6ee7a1227ef6b61a
dv-Termux <g.h.com>
2022-03-23 10:00:21 +0000

    merging contents from both versions of termux

---
+++ bash/upk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Tiltle: UPK aid app
+
+
+# Load upk-ascii-logo
+clear
+cat ./all/img/upk-ascii-text
+
+
+TERM_ROWS="$(tput rows)"
+TERM_COLS="$(tput cols)"
+
+echo "$TERM_ROWS, $TERMCOLS"
