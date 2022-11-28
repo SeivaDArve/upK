0628c1d4178c66f408f9d30e7e9d0e06a30794fb
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2022-11-28 23:23:42 +0000

    Adicionado um exemplo de como aceder a info sobre portaas (usando bash alias)

--- upk.sh
+++ upk.sh
@@ -2,4 +2,9 @@
 
 function f_call_drya {
 	# All software by Seiva D'Arve is connected by a 'package manager' called drya. Each app may be a simple app, but it connects to all other mainbapps. So, every app may ask drya for help. By running this function you will copy drya to your device without installing it and without modifying anything
+   echo j
 }
+
+
+# Links para descrever todas asportas do shopping:
+   alias 'PRT.C0.020'="echo \"Parque Central 0\""
