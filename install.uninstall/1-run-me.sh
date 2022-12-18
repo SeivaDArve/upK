4de5095f7cf4c540f0caed45b518dfadad6c5bc3
Seiva DArve <flowreshe.seiva.d.arve@gmail.com>
2022-12-18 06:58:58 +0000

    Creation of possibility to clone DRYa

---
+++ install.uninstall/1-run-me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# If DRYa is not installed to take care of this repository, then manually add to ~/.bashrc an alias to call for drya if needed
+   echo "alias upk-clone-drya="git clone https://github.com/SeivaDArve/DRYa.git" >> ~/.bashrc
