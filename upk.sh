15079fa85c22ea573e47311eeb06432e89e1f74c
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2023-02-25 06:51:57 +0000

    Merged: upk.sh + better CDI description

--- upk.sh
+++ upk.sh
@@ -9,6 +9,10 @@ function f_call_drya {
 # Links para descrever todas asportas do shopping:
    alias 'PRT.C0.020'="echo \"Parque Central 0\""
 
+if [ -z $1 ]; then
+   echo "upk: O que deseja fazer?"
+   #less readme
+
 if [ -z "$*" ]; then
    # Do something else if there are no arguments
       echo "upK app: No arguments where given"
@@ -24,10 +28,22 @@ elif [ $1 == "encrypt" ]; then
 elif [ $1 == "decrypt" ]; then
    # Misplace characters from order 1 to order 2. You may use order 2 as the readable order
 
-elif [[ $1 == "1" ]]; then
-   echo 1 arg 
-
 elif [ $1 == "1" ] && [ -z $2 ]; then
    echo 1 arg 
    echo no arg 2
+
+elif [ $1 == "img" ]; then
+
+   if [[ $2 == "planta" ]]; then
+      if [[ $3 == "cota-0" ]]; then
+         if [[ $4 == "no-guides" ]]; then
+            # xdg-open planta x
+            echo
+            #uDev: Para se poder alterar a ordem destes argumentos à vontade, convem criar um evento com flags
+         fi
+      fi
+   fi
+else
+   echo "upk: uDev: Description"
+
 fi
