78621d40ff864ed930c0ec1c92562c45b93cdfef
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-01-04 02:49:12 +0000

    Imp: File Structure

--- upk.sh
+++ upk.sh
@@ -120,6 +120,73 @@ function f_diary_help {
 }
 
 
+function f_range_dir_creator {
+   # Description: When there are lots of images to send AND sort from phone to PC, this script creates all directoies needed. 
+
+   # Use: specify a range of days you want this script to create directories 
+      # Example: enter: '20 31' and it will create:
+         # 20 Dez 2022
+         # 21 Dez 2022
+         # 22 Dez 2022
+         # 23 Dez 2022
+         # 24 Dez 2022
+         # 25 Dez 2022
+         # 26 Dez 2022
+         # 27 Dez 2022
+         # 28 Dez 2022
+         # 29 Dez 2022
+         # 30 Dez 2022
+         # 31 Dez 2022
+         # (all this at internal storage on android phone)
+
+   v_uname=$(uname -a)
+         
+   # When running script, a directory must be specified, unless it is Androi
+      # In case of Android, create always at interanal storage
+      
+      if [[ $v_uname =~ "Android" ]]; then
+         # If devide is Android, create necessary directories for the script to work
+         mkdir -p /sdcard/termux/ranged-dirs
+         
+         # ask for range
+         read -p "Range dir Creator: input low ranged number: " v_low
+         read -p "Range dir Creator: input high ranged number: " v_high
+         read -p "Range dir Creator: input month (Example: Sep): " v_month
+
+         for i in $(seq $v_low $v_high); do
+            echo "pasta-$i-$v_month-2025"
+            i=$((i+1))
+         done
+
+      elif [[ $v_uname =~ "Microsoft" ]]; then
+         clear
+
+         echo "You are not at Android"
+         echo "Dirs will be created at your current dir"
+         echo
+
+         # ask for range
+         read -p "Range dir Creator: input low ranged number: " v_low
+         read -p "Range dir Creator: input high ranged number: " v_high
+
+         # Create half of the name to be specified for the directory that is going to be created
+            v_month_year=$(date +%b-%Y)
+
+         if [[ $v_low > $v_high ]]; then
+            echo " > Must input low range before high range; aborting"
+
+         else
+            while true; do
+               mkdir -p "${v_low}-${v_month_year}"
+               v_low=$((v_low+1))
+            
+               if [[ $v_low > $v_high ]]; then break; fi
+            done
+         fi
+
+      fi
+
+}
 
 
 
@@ -369,6 +436,8 @@ elif [ $1 == "at" ] || [ $1 == "AT" ] || [ $1 == "At" ]; then
    # Pesquisa de ATs do Centro VG
    f_search_AT
 
+elif [ $1 == "range-dirs" ]; then
+   f_range_dir_creator  # uDev: runs on Android and Windows, but not on Linux
 
 elif [ $1 == "+" ] || [ $1 == "entrada" ]; then
    # Registar entrada no Vasco da Gama 
